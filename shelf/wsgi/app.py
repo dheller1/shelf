d40bc3ed028ac7dbf57d09610c715ca10d32d141
Dominik Heller <dominik.heller1@gmail.com>
2022-11-07 15:57:57 +0100

    integrate Flask, add list of books view and DB model for Author and Book (unrelated yet)

---
+++ shelf/wsgi/app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(_basedir, 'shelf.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'Author({self.name})'
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    subtitle = db.Column(db.String(200))
+    edition = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f'Book({self.title})'
+
+
+
+@app.route('/list')
+def index():
+    books = Book.query.all()
+    return render_template('list.html', books=books)
