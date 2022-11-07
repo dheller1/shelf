6a3cd7f945643e34506f9f7c48693b007a6ca00b
Dominik Heller <dominik.heller1@gmail.com>
2022-11-07 22:48:30 +0100

    add add.html template and the possibility to upload PDFs to add a new book

--- shelf/wsgi/app.py
+++ shelf/wsgi/app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+# from . import commands
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -29,7 +31,49 @@ class Book(db.Model):
     def __repr__(self):
         return f'Book({self.title})'
 
+UPLOAD_FOLDER = os.path.join(os.getenv('USERPROFILE'), 'shelf', 'uploads')
 
+
+def add_book(db, request):
+    if 'book_file' not in request.files:
+        flash('No file!')
+        redirect(request.url)
+        return None
+    file = request.files['book_file']
+
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        redirect(request.url)
+        return None
+
+    ext = os.path.splitext(file.filename)[1].lower()
+    if file and ext == '.pdf':
+        new_book = Book(title='TEMP_UNKNOWN')
+        db.session.add(new_book)
+        db.session.commit()
+
+        id = new_book.id
+        print('added new book with id:', id)
+        target_dir = os.path.join(UPLOAD_FOLDER, str(id))
+        if not os.path.isdir(target_dir):
+            os.mkdir(target_dir)
+        file.save(os.path.join(target_dir, file.filename))
+
+        return new_book
+    return None
+
+@app.route('/add', methods=('GET', 'POST'))
+def view_add():
+    if request.method == 'POST':
+        b = add_book(db, request)
+        if b is None:
+            return render_template('add.html')
+        else:
+            return redirect(url_for('view_book', book_id=b.id))
+    else:
+        return render_template('add.html')
 
 @app.route('/list')
 def view_list():
