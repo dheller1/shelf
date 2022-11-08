8803591e28a9b7be9bdc18bc40f87c4f4d1d3b4d
Dominik Heller <dominik.heller1@gmail.com>
2022-11-08 21:34:52 +0100

    add Author/Book relationship via the written_by table

--- shelf/db/book.py
+++ shelf/db/book.py
@@ -1,10 +1,12 @@
 from . import db
+from .written_by import written_by
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     subtitle = db.Column(db.String(200))
     edition = db.Column(db.String(100))
+    authors = db.relationship('Author', secondary=written_by, lazy='subquery', backref=db.backref('books', lazy=True))
 
     def __repr__(self):
         return f'Book({self.title})'
---
+++ shelf/db/written_by.py
@@ -0,0 +1,6 @@
+from . import db
+
+written_by = db.Table('written_by',
+    db.Column('author_id', db.Integer, db.ForeignKey('author.id'), primary_key=True),
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True)
+)
